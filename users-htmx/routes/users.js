eb64aff28aa89e95d0aafd0453b7c8550644ce79
Anyeel <angelmillanaragon@hotmail.com>
2025-04-10 22:11:27 +0200

    list and table added

--- users-htmx/routes/users.js
+++ users-htmx/routes/users.js
@@ -10,7 +10,38 @@ const router = express.Router();
 // La respuesta se envía en formato JSON
 router.get("/users", (req, res) => {
     const users = getAllUsers();
-    res.json(users);
+    const format = req.query.format;
+
+    if (format === "list") {
+        const listHTML = users.map(user => `<li>${user.nombre}</li>`).join("");
+        res.send(`<ul>${listHTML}</ul>`);
+    } else if (format === "table") {
+        const tableHTML = `
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Contraseña</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${users.map(user => `
+                        <tr>
+                            <td>${user.id}</td>
+                            <td>${user.nombre}</td>
+                            <td>${user.pass}</td>
+                        </tr>
+                    `).join("")}
+                </tbody>
+            </table>
+        `;
+        res.send(tableHTML);
+    } else if (format === "json") {
+        res.json(users);
+    } else {
+        res.status(400).send("Formato no soportado");
+    }
 });
 
 // Endpoint para añadir un usuario
